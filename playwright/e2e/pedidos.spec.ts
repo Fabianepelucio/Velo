86ae4ae9ccb2d0b4f5e7f31d40bdd531f0917e39
Fabianepelucio <fabianepelucio@yahoo.com.br>
2026-05-05 16:30:48 +0100

    AAA - Arrange - Act - Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from '@playwright/test';
-
+/// AAA - Arrenge - Act - Assert
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/');
   await page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' }).click();
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  git add.//Act
   await page.getByTestId('search-order-id').fill('VLO-KLWCX6');
   await page.getByTestId('search-order-button').click();
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-KLWCX6');
   await expect(page.getByTestId('order-result-status')).toBeVisible();
